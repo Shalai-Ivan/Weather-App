f3c6ae30226bbdcd2abb56e4ec719a5ce165d4d2
shalai-ivan <shalai.ivan30@gmail.com>
2022-09-24 19:21:24 +0300

    Interface

---
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by MacMini on 24.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var cloudsImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTmprLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func tapSearchImage(_ sender: Any) {
+    }
+}
+
